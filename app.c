6fa191f4ccff1b192bd14166bb19a00e58e9efe4
Sisart003 <chrishart.estrada@gmail.com>
2025-04-02 13:12:48 +0800

    Scanf functions

--- app.c
+++ app.c
@@ -4,6 +4,7 @@ int main(void)
 {
 
 
+
     return 0;
 }
 // C Basics
@@ -40,3 +41,15 @@ int main(void)
 
 // char str[] = "Hello World!";
 // printf("string: %s\n", str);
+
+
+// Scanf function is used to read formatted input from the console.
+// double n = 0;
+// printf("Enter a float: ");
+// scanf("%f", &n);
+// printf("n: %f\n", n);
+
+// char buffer[50];
+// printf("Enter a string: ");
+// scanf("%s", buffer);
+// printf("You entered: %s\n", buffer);
