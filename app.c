50e7652a47329e7247a869b116dd566b68479eb8
Sisart003 <chrishart.estrada@gmail.com>
2025-04-02 12:56:59 +0800

    The Printf function

--- app.c
+++ app.c
@@ -2,25 +2,41 @@
 
 int main(void)
 {
-    int position = 10;
-    int x, y, z;
 
-    x = 20;
-    y = 30;
-    z = 10;
-
-    float myNum = 5.99;
-    char myLetter = 'D';
-    double myNum2 = 9.99;
-    char let = 165;
-
-    printf("My number: %f\n", myNum);
-    printf("My letter: %c\n", myLetter);
-    printf("My number2: %lf\n", myNum2);
-    printf("My letter2: %c\n", let);
-
-    printf("Position: %d\n", position);
-    printf("x: %d,\t y: %d,\t z:%d\t", x, y, z);
 
     return 0;
 }
+// C Basics
+// int position = 10;
+// int x, y, z;
+
+// x = 20;
+// y = 30;
+// z = 10;
+
+// float myNum = 5.99;
+// char myLetter = 'D';
+// double myNum2 = 9.99;
+// char let = 165;
+
+// printf("My number: %f\n", myNum);
+// printf("My letter: %c\n", myLetter);
+// printf("My number2: %lf\n", myNum2);
+// printf("My letter2: %c\n", let);
+
+// printf("Position: %d\n", position);
+// printf("x: %d,\t y: %d,\t z:%d\t", x, y, z);
+
+// The printf function is used to print formatted output to the console.
+// int x = 20;
+// printf("Hello World!\n");
+// printf("int: %d\n", x+2);
+
+// char c = 'F';
+// printf("char: %c\n", c);
+
+// double d = 6.9420;
+// printf("double: %lf\n", d);
+
+// char str[] = "Hello World!";
+// printf("string: %s\n", str);
