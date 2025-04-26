5cfaca69737a6e11b5ee8915bfeb1b9b88ecb670
Sisart003 <chrishart.estrada@gmail.com>
2025-04-26 11:16:47 +0800

    Pass by Reference

--- app.c
+++ app.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
-// #include <stdlib.h>
-// #include <ctype.h>
-// #include <string.h>
-// #include <stdbool.h>
+
+void swap(int a, int b);
+void swap2(int *a, int *b);
 
 int main(void)
 {
+	int x = 5;
+	int y = 10;
+	
+	swap(x, y);
+	printf("x: %d\ny: %d\n", x, y);
+	
+	swap2(&x, &y);
+	printf("x: %d\ny: %d\n", x, y);
+	
+	return 0;
+}
 
-    return 0;
+void swap(int a, int b)
+{
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+void swap2(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
